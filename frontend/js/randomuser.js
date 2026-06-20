f91c2f60d03adfbef9bdc779177d8c9a6a17f743
Namaswitha <your-email@example.com>
2026-06-20 10:32:24 +0530

    randomuser completed

---
+++ frontend/js/randomuser.js
@@ -0,0 +1,19 @@
+function randomuser(){
+    fetch("https://randomuser.me/api/")
+    .then(function(res){
+        return res.json();
+
+    })
+    .then(function(data){
+        var user=data.results[0];
+        var fullname=user.name.title+" "+user.name.first+" "+user.name.last;
+        document.getElementById("user-img").src=user.picture.large;
+
+    document.getElementById("user-name").innerText=user.fullname;
+    document.getElementById("user-gender").innerText=user.gender;
+    })
+    .catch(function(err){
+        console.log("Error :"+err);
+
+    })
+}
